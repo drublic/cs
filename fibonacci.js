c9f3073aec0256a0feaa2cdfd43aa5dab5c967bf
Alex Vernacchia <avernacchia@exacttarget.com>
2013-05-21 10:14:22 -0400

    Better use of Big O theory. Less computation required, less memory eaten up

--- fibonacci.js
+++ fibonacci.js
@@ -11,3 +11,25 @@ var fib = function (all) {
 };
 
 console.log(fib([1,1]));
+
+// Better Fibonacci, less computing required. Better O(n)
+var fib2 = function( n, index ) {
+	var results = [0, 1];
+
+	if( n < 3 ) {
+		return results[ n - 1 ];
+	}
+
+	for( var i = 2; i < n; i++ ){
+		results[ i ] = results[ i - 2 ] + results[ i - 1 ];
+	}
+
+	if( !!index ) {
+		return results[ index -1 ];
+	} else {
+		return results;
+	}
+};
+
+console.log( fib2( 10 ) );
+console.log( fib2( 10, 5 ) );
